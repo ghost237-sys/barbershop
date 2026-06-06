883f1886e16774732413bc5afc8c59feecd793b3
ghost237-sys <your-email@example.com>
2026-06-06 19:32:40 +0300

    fixed the asgi.py and requirements version problems

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,18 +1,14 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-import queue_app.routing  # we'll create this in Phase 4
+from channels.auth import AuthMiddlewareStack
+import queue_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
-    # Regular HTTP requests go through Django as normal
     'http': get_asgi_application(),
-
-    # WebSocket connections go through Channels
-    # AllowedHostsOriginValidator checks the origin header for security
-    'websocket': AllowedHostsOriginValidator(
+    'websocket': AuthMiddlewareStack(
         URLRouter(queue_app.routing.websocket_urlpatterns)
     ),
 })
